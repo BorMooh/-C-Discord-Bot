bf73d006ed02d10fd878fddfdb75a0cce416381c
BorMooh <bor.mohoric2@gmail.com>
2021-05-04 12:17:44 +0200

    Import

---
+++ DiscordBot/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace DiscordBot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //https://youtu.be/7-tyLCAO4mY?t=581
+            var Bot = new Bot();
+        }
+    }
+}
